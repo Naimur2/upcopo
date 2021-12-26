ed0f78a7b3c76de612e51e4ba85bae7929fb50af
naimur2 <naimurrahaman248@gmail.com>
2021-12-26 11:58:46 +0600

    updates

--- src/user-screens/userProfile/UserProfile.js
+++ src/user-screens/userProfile/UserProfile.js
@@ -1,24 +1,15 @@
-import { Box, Center, Text } from "native-base";
+import { Box, Center, Text, Image } from "native-base";
+import { Image as RnImage,Dimensions  } from "react-native";
 import React from "react";
-import BackgroundImage from "./SvgComponent";
-// import image52 from '../../assets/images/image52.svg';
 
-//const source = "../../../assets/images";
-//const image52 = require(source + "/image52.svg");
-//const image52Uri = RnImage.resolveAssetSource(image52).uri;
+const windowWidth = Dimensions.get('window').width;
+
+const source = "../../../assets/images";
+const image52 = require(source + "/image52.png");
+const image52Uri = RnImage.resolveAssetSource(image52).uri;
 
 export default function UserProfile({ route, navigation }) {
     const { userId } = route.params;
     // changes
-
-    return (
-        <Box height={"100%"} alignItems={"center"} justifyContent={"center"}>
-            <Text color="black" fontWeight={"bold"}>
-                User Profile {userId}
-            </Text>
-            <Center py={10}>
-                <BackgroundImage />
-            </Center>
-        </Box>
-    );
+    return <Image w={windowWidth} alt={userId} source={{ uri: image52Uri }} h={'30%'} />;
 }
