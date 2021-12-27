7568037582581bc0b88c41917c03b700b691edf8
naimur2 <naimurrahaman248@gmail.com>
2021-12-27 15:51:07 +0600

    updates

--- src/user-screens/userProfile/UserProfile.js
+++ src/user-screens/userProfile/UserProfile.js
@@ -1,8 +1,9 @@
-import { Avatar, Box, Image } from "native-base";
+import { Avatar, Box, Center, Image } from "native-base";
 import React from "react";
 import { Dimensions, Image as RnImage } from "react-native";
 
 const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
 
 const source = "../../../assets/images";
 const image52 = require(source + "/image52.png");
@@ -15,23 +16,21 @@ export default function UserProfile({ route, navigation }) {
     //const { userId } = route.params;
     // changes
     return (
-        <Box bg={"#161616"}>
-            <Image
-                w={windowWidth}
-                alt={'userId'}
-                source={{ uri: image52Uri }}
-                h={"50%"}
-                position={'relative'}
-            />
-            <Avatar
-            size={'xl'}
-            source={{uri:panda_05Uri}}
-            position={'absolute'}
-            bottom={0}
-
-            />
-             
-
-        </Box>
+        <Center>
+            <Box  w={'full'} h={windowHeight/4} position={'relative'}>
+                <Image
+                    alt={'userId'}
+                    source={{ uri: image52Uri }}
+                    h={"100%"}
+                    w={"100%"}
+                />
+            </Box>
+                <Avatar
+                size={'xl'}
+                source={{uri:panda_05Uri}}
+                position={'absolute'}
+                bottom={-50}
+                />
+        </Center>
     );
 }
