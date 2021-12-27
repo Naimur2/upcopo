622e57c3ef7cea7127e2bf7783fbd6824dc11651
Abdul Kader <abdulkadirshohan@gmail.com>
2021-12-27 18:12:13 +0600

    update

--- src/user-screens/userProfile/UserProfile.js
+++ src/user-screens/userProfile/UserProfile.js
@@ -19,7 +19,7 @@ export default function UserProfile({ route, navigation }) {
     //const { userId } = route.params;
     // changes
     return (
-        <Box>
+        <Box bg={'#f9f9f9'}>
             <Center>
                 <Box w={"full"} h={windowHeight / 3.4} position={"relative"}>
                     <Image
@@ -42,9 +42,27 @@ export default function UserProfile({ route, navigation }) {
                 </Card>
             </HStack>
             <HStack space={2} alignItems={'center'} justifyContent={'center'} py={1} px={4}>
-                <Text color={"#000"}>Jhon Doe</Text>
+                <Text color={"#000"} fontWeight={600}>Jhon Doe</Text>
                 <Varified h={6} w={6} />
             </HStack>
+
+
+      
+         <HStack space={1}>
+                <Card  space={1} flexDirection={'row'}  alignItems={'center'}  justifyContent={'space-between'}>
+
+                <Icon  bg={'#52B69A'} borderRadius={50} name={"search"} size={16} color={"#FFFFFF"} p={2}   />
+            
+                <Text fontFamily={'body'} color={'#000'}> 85208,Mesa, Az </Text>
+                <Box space={2}>
+                     <Icon name={'candle'}/>
+                </Box>
+
+                </Card>
+              
+            </HStack>
+      
+
         </Box>
     );
 }
