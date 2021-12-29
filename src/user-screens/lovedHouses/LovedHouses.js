0e4519f109c7c2c9d6e8fe23779f41d0969d5c9a
naimur2 <naimurrahaman248@gmail.com>
2021-12-29 15:37:33 +0600

    updates on loved houses

--- src/user-screens/lovedHouses/LovedHouses.js
+++ src/user-screens/lovedHouses/LovedHouses.js
@@ -6,76 +6,69 @@ export default function LovedHouses() {
     const lovedHouse = [
         {
             _id: "1",
-            imageUrl: 'https://thelens.news/app/uploads/2020/12/236-456x342.jpg',
-            address: '889 Palmeron Ave, Mcd..',
-            numOfBed: 2,
-            numOfBath: 3,
+            imageUrl:
+                "https://thelens.news/app/uploads/2020/12/236-456x342.jpg",
+            address: "889 Palmeron Ave, Mcd..",
+            numOfBed: 10,
+            numOfBath: 5,
             isSaved: true,
             price: 0.05686,
         },
         {
             _id: "2",
-            imageUrl: 'https://thelens.news/app/uploads/2020/12/236-456x342.jpg',
-            address: '889 Palmeron Ave, Mcd..',
+            imageUrl:
+                "https://thelens.news/app/uploads/2020/12/236-456x342.jpg",
+            address: "889 Palmeron Ave, Mcd..",
             numOfBed: 2,
             numOfBath: 3,
             isSaved: false,
             price: 0.05686,
         },
+        {
+            _id: "3",
+            imageUrl:
+                "https://thelens.news/app/uploads/2020/12/236-456x342.jpg",
+            address: "889 Palmeron Ave, Mcd..",
+            numOfBed: 10,
+            numOfBath: 5,
+            isSaved: true,
+            price: 0.05686,
+        },
+        {
+            _id: "4",
+            imageUrl:
+                "https://thelens.news/app/uploads/2020/12/236-456x342.jpg",
+            address: "889 Palmeron Ave, Mcd..",
+            numOfBed: 2,
+            numOfBath: 3,
+            isSaved: true,
+            price: 0.05686,
+        },
+    ];
 
-    ]
-const renderItem=({item})=>(
-    <LovedHousesCard
-    address={item.address}
-    numOfBed={item.numOfBed}
-    numOfBath={item.numOfBath}
-    isSaved={item.isSaved}
-    price={item.price}
-    imageUrl={item.imageUrl}
-    onPlaceBid={() => console.log('Place a bid')}
-/>
-)
+    const renderItem = ({ item }) => {
+        
+        if (!item.isSaved) return;
+        return (
+            <LovedHousesCard
+                address={item.address}
+                numOfBed={item.numOfBed}
+                numOfBath={item.numOfBath}
+                isSaved={item.isSaved}
+                price={item.price}
+                imageUrl={item.imageUrl}
+                onPlaceBid={() => console.log("Place a bid")}
+            />
+        );
+    };
+
     return (
         <FlatList
-        showsVerticalScrollIndicator={false}
-        showsHorizontalScrollIndicator={false}
-       
-        data={lovedHouse}
-        renderItem={renderItem}
-        keyExtractor={(item) => item._id}
-    />
-    /*
-     <Stack py={10} space={4}>
-            <LovedHousesCard
-                key={"a"}
-                address
-                numOfBed
-                numOfBath
-                isSaved
-                price
-                onPlaceBid={() => console.log('Place a bid')}
-            />
-        </Stack>
-    
-    */
-       
+            showsVerticalScrollIndicator={false}
+            showsHorizontalScrollIndicator={false}
+            data={lovedHouse}
+            renderItem={renderItem}
+            keyExtractor={(item) => item._id}
+        />
     );
 }
-
-/*
-
-   <HStack space={6}>
-                <HStack alignItems={'center'} >
-                    <Icon name={'bed-filled'} size={20} color={'#889096'} />
-                    <Text color={'#687076'} fontFamily={'body'} fontSize={12}>3 bed</Text>
-                </HStack>
-                <HStack space={2} alignItems={'center'} >
-                    <Icon name={'bath'} size={20} color={'#889096'} />
-                    <Text color={'#687076'} fontFamily={'body'} fontSize={12}>2 bath</Text>
-                </HStack>
-                <HStack space={2} alignItems={'center'} >
-                    <Icon name={'heart-dark'} size={20} color={'#000'} />
-                    <Text color={'#687076'} fontFamily={'body'} fontSize={12}>Saved</Text>
-                </HStack>
-
-*/
--- src/user-screens/lovedHouses/components/LovedHousesCard.js
+++ src/user-screens/lovedHouses/components/LovedHousesCard.js
@@ -3,17 +3,25 @@ import React from "react";
 import IconWithText from "../../../utility/IconWithText";
 import UtilityBtn from "../../../utility/UtilityBtn";
 
-export default function LovedHousesCard({onPlaceBid,ImageUrl,address,numOfBed,numOfBath,isSaved,price}) {
+export default function LovedHousesCard({
+    onPlaceBid,
+    imageUrl,
+    address,
+    numOfBed,
+    numOfBath,
+    isSaved,
+    price,
+}) {
     const icons = [
         {
             _id: "1",
-            text: "3 bed",
+            text: `${numOfBed} bed`,
             icon: "bed-filled",
             iconColor: "#889096",
         },
         {
             _id: "2",
-            text: "2 bath",
+            text: `${numOfBath} bath`,
             icon: "bath-filled",
             iconColor: "#889096",
             iconStyle: { size: 15 },
@@ -28,71 +36,59 @@ export default function LovedHousesCard({onPlaceBid,ImageUrl,address,numOfBed,nu
     ];
 
     return (
-       
-            <HStack
-                justifyContent={"space-between"}
-                px={4}
-                alignItems={"center"}
+        <HStack mb={1} justifyContent={"space-between"} px={4} alignItems={"center"}>
+            <Box w="26%" h="115" overflow="hidden">
+                <Image
+                    borderRadius={14}
+                    alt="Loved Houses"
+                    width={"100%"}
+                    height={"100%"}
+                    source={{
+                        uri: imageUrl,
+                    }}
+                />
+            </Box>
+            <VStack
+                w="70%"
+                space="3"
+                borderBottomColor={"#E6E8EB"}
+                borderBottomWidth={1}
+                py={4}
             >
-                <Box w="26%" h="115" overflow="hidden">
-                    <Image
-                        borderRadius={14}
-                        alt="Loved Houses"
-                        width={"100%"}
-                        height={"100%"}
-                        source={{
-                            uri:ImageUrl,
+                {/* define maximum character */}
+                <Text
+                    fontFamily={"body"}
+                    fontWeight={"500"}
+                    color={"#3D454A"}
+                    fontSize={"18"}
+                >
+                    {address}
+                </Text>
+                <HStack justifyContent={"space-between"} alignItems={"center"}>
+                    {icons.map((icon) => (
+                        <IconWithText
+                            key={icon._id}
+                            iconName={icon.icon}
+                            iconColor={icon.iconColor}
+                            text={icon.text}
+                            iconStyle={icon.iconStyle || ""}
+                        />
+                    ))}
+                </HStack>
+                <HStack justifyContent={"space-between"} alignItems={"center"}>
+                    <IconWithText
+                        iconName="etherium"
+                        text={price}
+                        iconColor={"#52B69A"}
+                        textStyle={{
+                            fontWeight: 700,
+                            fontFamily: "body",
+                            color: "#52B69A",
                         }}
                     />
-                </Box>
-                <VStack
-                    w="70%"
-                    space="3"
-                    borderBottomColor={"#E6E8EB"}
-                    borderBottomWidth={1}
-                    py={4}
-                >
-                    {/* define maximum character */}
-                    <Text
-                        fontFamily={"body"}
-                        fontWeight={"500"}
-                        color={"#3D454A"}
-                        fontSize={"18"}
-                    >
-                       {address}
-                    </Text>
-                    <HStack
-                        justifyContent={"space-between"}
-                        alignItems={"center"}
-                    >
-                        {icons.map((icon) => (
-                            <IconWithText
-                                key={icon._id}
-                                iconName={icon.icon}
-                                iconColor={icon.iconColor}
-                                text={icon.text}
-                                iconStyle={icon.iconStyle || ""}
-                            />
-                        ))}
-                    </HStack>
-                    <HStack
-                        justifyContent={"space-between"}
-                        alignItems={"center"}
-                    >
-                        <IconWithText
-                            iconName="etherium"
-                            text="0.05686"
-                            iconColor={"#52B69A"}
-                            textStyle={{
-                                fontWeight: 700,
-                                fontFamily: "body",
-                                color: "#52B69A",
-                            }}
-                        />
-                        <UtilityBtn title={"Place a Bid"} onPress={onPlaceBid} />
-                    </HStack>
-                </VStack>
-            </HStack>
+                    <UtilityBtn title={"Place a Bid"} onPress={onPlaceBid} />
+                </HStack>
+            </VStack>
+        </HStack>
     );
 }
-
