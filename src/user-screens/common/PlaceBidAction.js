6b6a0078c7a873b929a8b4893076a8028b7680ba
naimur2 <naimurrahaman248@gmail.com>
2022-01-08 09:57:04 +0600

    updates

--- src/user-screens/common/PlaceBidAction.js
+++ src/user-screens/common/PlaceBidAction.js
@@ -1,25 +1,24 @@
 import { Actionsheet, Box, Text } from "native-base";
 import React from "react";
+import KeyBoardView from "../../utility/KeyBoardView";
+
 export default function PlaceBidAction({ isOpen, onOpen, onClose }) {
     return (
-        <Actionsheet  isOpen={isOpen} onOpen={onOpen} onClose={onClose}>
-            <Actionsheet.Content bg={'amber.100'}>
-                <Box w="100%" h={60} px={4} justifyContent="center">
-                    <Text
-                        fontSize="16"
-                        color="gray.500"
-                        _dark={{
-                            color: "light.400",
-                        }}
-                    >
-                        Albums
-                    </Text>
-                </Box>
-                {/* <Actionsheet.Item>Delete</Actionsheet.Item>
-                <Actionsheet.Item>Share</Actionsheet.Item>
-                <Actionsheet.Item>Play</Actionsheet.Item>
-                <Actionsheet.Item>Favourite</Actionsheet.Item>
-                <Actionsheet.Item>Cancel</Actionsheet.Item> */}
+        <Actionsheet isOpen={isOpen} onOpen={onOpen} onClose={onClose}>
+            <Actionsheet.Content bg={"#ffff"}>
+                <KeyBoardView>
+                    <Box w="100%" h={60} px={4} justifyContent="center">
+                        <Text
+                            fontSize="16"
+                            color="gray.500"
+                            _dark={{
+                                color: "light.400",
+                            }}
+                        >
+                            Albums
+                        </Text>
+                    </Box>
+                </KeyBoardView>
             </Actionsheet.Content>
         </Actionsheet>
     );
