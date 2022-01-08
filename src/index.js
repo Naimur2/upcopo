a83c5a97589e906841006046b5a73f72de16411d
Abdul Kader <abdulkadirshohan@gmail.com>
2022-01-08 12:40:55 +0600

    cc

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import React from "react";
 import ThemeConfig from "../theme-config";
 import DrawerNavigator from "./DrawerNavigator";
 
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> f68277b014449dbf5a0fbde4cce8245620244959
 export default function index() {
     // authentication  will be done here
     return (
