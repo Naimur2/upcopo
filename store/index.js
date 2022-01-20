8a4638c5608d4e70f46f25cc23652a799ab737c2
naimur2 <naimurrahaman248@gmail.com>
2022-01-20 09:56:15 +0600

    updates

--- store/index.js
+++ store/index.js
@@ -1,10 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
 import recentSearchReducer from "./slices/recentSearchSlice";
-import uiReducer from './slices/uiSlice'
+import uiReducer from "./slices/uiSlice";
+import messagesReducer from "./slices/messagesSlice";
 
 const store = configureStore({
-    reducer: { auth: authReducer, recentSearch: recentSearchReducer,ui:uiReducer },
+    reducer: {
+        auth: authReducer,
+        recentSearch: recentSearchReducer,
+        ui: uiReducer,
+        messages:messagesReducer,
+    },
 });
 
 export default store;
---
+++ store/slices/messagesSlice.js
@@ -0,0 +1,122 @@
+import { createSlice } from "@reduxjs/toolkit";
+const messages = [
+    {
+        _id: "01",
+        from: {
+            _id: "100",
+            username: "Jrny club",
+            avatarUrl:
+                "https://thelens.news/app/uploads/2020/12/236-456x342.jpg",
+            isActive: true,
+            seen: true,
+        },
+        to: {
+            _id: "200",
+            username: "Crypto punks",
+            avatarUrl:
+                "https://thelens.news/app/uploads/2020/12/236-456x342.jpg",
+            isActive: true,
+            seen: false,
+        },
+        message: "Welcome Home",
+        date: "January 18,2022,18:22:22",
+        timestamp: "1642579204802",
+    },
+    {
+        _id: "02",
+        from: {
+            _id: "100",
+            username: "Jrny club",
+            avatarUrl:
+                "https://thelens.news/app/uploads/2020/12/236-456x342.jpg",
+            isActive: true,
+            seen: true,
+        },
+        to: {
+            _id: "200",
+            username: "Crypto punks",
+            avatarUrl:
+                "https://thelens.news/app/uploads/2020/12/236-456x342.jpg",
+            isActive: true,
+            seen: false,
+        },
+        message: "Hello World",
+        date: "January 18,2022,18:22:22",
+        timestamp: "1642610122256",
+    },
+    {
+        _id: "03",
+        from: {
+            _id: "300",
+            username: "Jrny club",
+            avatarUrl:
+                "https://thelens.news/app/uploads/2020/12/236-456x342.jpg",
+            isActive: true,
+            seen: true,
+        },
+        to: {
+            _id: "100",
+            username: "Crypto punks",
+            avatarUrl:
+                "https://thelens.news/app/uploads/2020/12/236-456x342.jpg",
+            isActive: true,
+            seen: false,
+        },
+        message: "Hey, Jol, If yo’re free now we can..",
+        date: "January 18,2022,18:22:22",
+        timestamp: "1642610338558",
+    },
+    {
+        _id: "04",
+        from: {
+            _id: "300",
+            username: "Jrny club",
+            avatarUrl:
+                "https://thelens.news/app/uploads/2020/12/236-456x342.jpg",
+            isActive: true,
+            seen: true,
+        },
+        to: {
+            _id: "100",
+            username: "Crypto punks",
+            avatarUrl:
+                "https://thelens.news/app/uploads/2020/12/236-456x342.jpg",
+            isActive: true,
+            seen: true,
+        },
+        message: "Hello World",
+        date: "January 18,2022,18:22:22",
+        timestamp: "1642610338558",
+    },
+];
+
+const initialMessageState = {
+    messages: [],
+};
+
+const messageSlice = createSlice({
+    name: "messages",
+    initialState: initialMessageState,
+    reducers: {
+        addMessages: (state, action) => {
+            state.messages = action.payload;
+        },
+        sendMessage: (state, action) => {
+            console.log(action.payload);
+        },
+    },
+});
+
+export const messageActions=messageSlice.actions;
+export default messageSlice.reducer;
+
+
+export const getMessages = () => {
+    return (dispatch) => {
+        dispatch(messageActions.addMessages(messages));
+    };
+};
+
+
+
+
