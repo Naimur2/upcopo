9ee46d6cb7a79f6fa32aad7ccc50057d2bfd90a4
abdulkadirshohan <abdulkadirshohan@gmail.com>
2022-01-20 14:43:31 +0600

    data

---
+++ arraydata/houseData.js
@@ -0,0 +1,203 @@
+const houseData = [
+    {
+        _id: '1',
+        houseName: 'Turn Key House',
+        houseImgUrl: 'https://i.ibb.co/cY428GM/Rectangle-13.png',
+        houseOwnerName: 'Luckas',
+        houseOwnDate: 'W.year 1942',
+        deadline: 'Jan 10,2022,10:10:10 ',
+        houseDescriptions: `Simple house with modern architecture and cool
+        interiors located in the city crnter making easier
+        for you to access`,
+        houseLength: '3,160',
+        houseAddress: '1523 N 106th St, Mesa, AZ 85207',
+        houseAddress1: '2715 La Cuesta Dr',
+        houseAddress2: 'Los Angeles, CA 90046',
+        numberOfBed: '6',
+        numberOfBath: '3',
+        topBid: '0.08',
+        LastBid: '0.008',
+        currentBid: '3.40',
+        actionTime: '2h:40m',
+        isLoved: ''
+    },
+    {
+        _id: '2',
+        houseName: 'Capstone house',
+        houseImgUrl: 'https://i.ibb.co/rcyqj2L/Thumbnail-1.png',
+        houseOwnerName: 'Luckas',
+        houseOwnDate: 'W.year 1942',
+        deadline: 'Jan 10,2022,10:10:10 ',
+        houseDescriptions: `Simple house with modern architecture and cool
+        interiors located in the city crnter making easier
+        for you to access`,
+        houseLength: '3,160',
+        houseAddress: '1523 N 106th St, Mesa, AZ 85207',
+        houseAddress1: '953 N orange Grove Ave',
+        houseAddress2: 'West Hollywood, CA 90046',
+        numberOfBed: '6',
+        numberOfBath: '3',
+        topBid: '0.08',
+        LastBid: '0.008',
+        currentBid: '2.96',
+        actionTime: '2h:40m',
+        isLoved: ''
+    },
+    {
+        _id: '3',
+        houseName: 'Turn Key House',
+        houseImgUrl: 'https://i.ibb.co/RzpQ9wj/Thumbnail.png',
+        houseOwnerName: 'Luckas',
+        houseOwnDate: 'W.year 1942',
+        deadline: 'Jan 10,2022,10:10:10 ',
+        houseDescriptions: `Simple house with modern architecture and cool
+        interiors located in the city crnter making easier
+        for you to access`,
+        houseLength: '3,160',
+        houseAddress: '1523 N 106th St, Mesa, AZ 85207',
+        houseAddress1: '953 N orange Grove Ave',
+        houseAddress2: 'West Hollywood, CA 90046, USA',
+        numberOfBed: '6',
+        numberOfBath: '3',
+        topBid: '0.08',
+        LastBid: '0.008',
+        currentBid: '1.08',
+        actionTime: '2h:40m',
+        isLoved: ''
+    },
+    {
+        _id: '4',
+        houseName: 'Turn Key House',
+        houseImgUrl: 'https://i.ibb.co/kg08WXS/image-54.png',
+        houseOwnerName: 'Luckas',
+        houseOwnDate: 'W.year 1942',
+        deadline: 'Jan 10,2022,10:10:10 ',
+        houseDescriptions: `Simple house with modern architecture and cool
+        interiors located in the city crnter making easier
+        for you to access`,
+        houseLength: '3,160',
+        houseAddress: '1523 N 106th St, Mesa, AZ 85207',
+        houseAddress1: '953 N orange Grove Ave',
+        houseAddress2: 'West Hollywood, CA 90046, USA',
+        numberOfBed: '6',
+        numberOfBath: '3',
+        topBid: '0.08',
+        LastBid: '0.008',
+        currentBid: '1.08',
+        actionTime: '2h:40m',
+        isLoved: ''
+    }
+    ,
+    {
+        _id: '5',
+        houseName: 'Turn Key House',
+        houseImgUrl: 'https://i.ibb.co/k0Sm9Fs/image-54-1.png',
+        houseOwnerName: 'Luckas',
+        houseOwnDate: 'W.year 1942',
+        deadline: 'Jan 10,2022,10:10:10 ',
+        houseDescriptions: `Simple house with modern architecture and cool
+        interiors located in the city crnter making easier
+        for you to access`,
+        houseLength: '3,160',
+        houseAddress: '1523 N 106th St, Mesa, AZ 85207',
+        houseAddress1: '953 N orange Grove Ave',
+        houseAddress2: 'West Hollywood, CA 90046, USA',
+        numberOfBed: '6',
+        numberOfBath: '3',
+        topBid: '0.08',
+        LastBid: '0.008',
+        currentBid: '1.08',
+        actionTime: '2h:40m',
+        isLoved: ''
+    }
+    ,
+    {
+        _id: '6',
+        houseName: 'Turn Key House',
+        houseImgUrl: 'https://i.ibb.co/2W7tv2k/image-54-2.png',
+        houseOwnerName: 'Luckas',
+        houseOwnDate: 'W.year 1942',
+        deadline: 'Jan 10,2022,10:10:10 ',
+        houseDescriptions: `Simple house with modern architecture and cool
+        interiors located in the city crnter making easier
+        for you to access`,
+        houseLength: '3,160',
+        houseAddress: '1523 N 106th St, Mesa, AZ 85207',
+        houseAddress1: '953 N orange Grove Ave',
+        houseAddress2: 'West Hollywood, CA 90046, USA',
+        numberOfBed: '6',
+        numberOfBath: '3',
+        topBid: '0.08',
+        LastBid: '0.008',
+        currentBid: '1.08',
+        actionTime: '2h:40m',
+        isLoved: ''
+    },
+    {
+        _id: '7',
+        houseName: 'Turn Key House',
+        houseImgUrl: 'https://i.ibb.co/31LCwD2/image-54-3.png',
+        houseOwnerName: 'Luckas',
+        houseOwnDate: 'W.year 1942',
+        deadline: 'Jan 10,2022,10:10:10 ',
+        houseDescriptions: `Simple house with modern architecture and cool
+        interiors located in the city crnter making easier
+        for you to access`,
+        houseLength: '3,160',
+        houseAddress: '1523 N 106th St, Mesa, AZ 85207',
+        houseAddress1: '953 N orange Grove Ave',
+        houseAddress2: 'West Hollywood, CA 90046, USA',
+        numberOfBed: '6',
+        numberOfBath: '3',
+        topBid: '0.08',
+        LastBid: '0.008',
+        currentBid: '1.08',
+        actionTime: '2h:40m',
+        isLoved: ''
+    },
+    {
+        _id: '8',
+        houseName: 'Turn Key House',
+        houseImgUrl: 'https://i.ibb.co/Pcg8dym/image-54-4.png',
+        houseOwnerName: 'Luckas',
+        houseOwnDate: 'W.year 1942',
+        deadline: 'Jan 10,2022,10:10:10 ',
+        houseDescriptions: `Simple house with modern architecture and cool
+        interiors located in the city crnter making easier
+        for you to access`,
+        houseLength: '3,160',
+        houseAddress: '1523 N 106th St, Mesa, AZ 85207',
+        houseAddress1: '953 N orange Grove Ave',
+        houseAddress2: 'West Hollywood, CA 90046, USA',
+        numberOfBed: '6',
+        numberOfBath: '3',
+        topBid: '0.08',
+        LastBid: '0.008',
+        currentBid: '1.08',
+        actionTime: '2h:40m',
+        isLoved: ''
+    }
+    ,
+    {
+        _id: '9',
+        houseName: 'Turn Key House',
+        houseImgUrl: 'https://i.ibb.co/31LCwD2/image-54-3.png',
+        houseOwnerName: 'Luckas',
+        houseOwnDate: 'W.year 1942',
+        deadline: 'Jan 10,2022,10:10:10 ',
+        houseDescriptions: `Simple house with modern architecture and cool
+        interiors located in the city crnter making easier
+        for you to access`,
+        houseLength: '3,160',
+        houseAddress: '1523 N 106th St, Mesa, AZ 85207',
+        houseAddress1: '953 N orange Grove Ave',
+        houseAddress2: 'West Hollywood, CA 90046, USA',
+        numberOfBed: '6',
+        numberOfBath: '3',
+        topBid: '0.08',
+        LastBid: '0.008',
+        currentBid: '1.08',
+        actionTime: '2h:40m',
+        isLoved: ''
+    }
+]
