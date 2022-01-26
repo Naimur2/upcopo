af0393182b4095170947c2e147747571573e0f5b
naimur2 <naimurrahaman248@gmail.com>
2022-01-26 09:59:31 +0600

    updates

--- src/drawer-screens/closed/components/wallet/TransectionPage.js
+++ src/drawer-screens/closed/components/wallet/TransectionPage.js
@@ -1,31 +1,72 @@
-import { Box, Center, Image, Modal } from "native-base";
+import { Box, Center, Image, Modal, VStack,Text } from "native-base";
 import React from "react";
-import { Dimensions, Image as RnImage } from "react-native";
+import { Image as RnImage } from "react-native";
 import Card from "../../../../utility/Card";
 const imgScan = require("../../../../../assets/images/wallet.png");
 const wallet = RnImage.resolveAssetSource(imgScan).uri;
 
 export default function TransectionPage({ isOPen, onClose, ...rest }) {
-    const {width}=Dimensions.get('window');
+    const [showView1,setShowView1] =React.useState(true);
+
+    const ViewOne = () => (
+        <VStack>
+            <Card>
+                <Center>
+                    <Box
+                        borderRadius={5}
+                        mt={2}
+                        mb={10}
+                        h={1.5}
+                        px="10"
+                        bg="#DFE3E6"
+                    />
+                    <Box w="70%" h="150">
+                        <Image
+                            alt={"QR code"}
+                            resizeMode="cover"
+                            source={{ uri: wallet }}
+                            h="full"
+                            w="full"
+                        />
+                    </Box>
+                </Center>
+            </Card>
+            <Text onPress={()=> setShowView1(false)} color="dark.200">Toggle</Text>
+        </VStack>
+    );
+    
+    const ViewTwo = () => (
+        <VStack>
+            <Card>
+                <Center>
+                    <Box
+                        borderRadius={5}
+                        mt={2}
+                        mb={10}
+                        h={1.5}
+                        px="10"
+                        bg="#DFE3E6"
+                    />
+                    <Box w="70%" h="150">
+                        <Image
+                            alt={"QR code"}
+                            resizeMode="cover"
+                            source={{ uri: wallet }}
+                            h="full"
+                            w="full"
+                        />
+                    </Box>
+                </Center>
+            </Card>
+            <Text onPress={()=> setShowView1(true)} color="dark.200">Close</Text>
+        </VStack>
+    );
 
     return (
         <Modal isOpen={isOPen} onClose={onClose} {...rest}>
             <Modal.Content bg={"#F9F9F9"} w={"full"} maxWidth="600px">
                 <Modal.Body>
-                    <Card>
-                        <Center>
-                            <Box borderRadius={5} mt={2} mb={10} h={1.5} px="10" bg="#DFE3E6"/>
-                            <Box w="70%" h="150" >
-                                <Image
-                                    alt={"QR code"}
-                                    resizeMode="cover"
-                                    source={{ uri: wallet }}
-                                    h="full"
-                                    w="full"
-                                />
-                            </Box>
-                        </Center>
-                    </Card>
+                   {showView1 ? <ViewOne /> :<ViewTwo />}
                 </Modal.Body>
             </Modal.Content>
         </Modal>
