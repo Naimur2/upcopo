2f4e971a648bce955632e4266739e803154a5cae
abdulkadirshohan <abdulkadirshohan@gmail.com>
2022-01-27 10:39:11 +0600

    update

--- src/user-screens/mapScreen/MapScreen.js
+++ src/user-screens/mapScreen/MapScreen.js
@@ -2,32 +2,33 @@ import { Center } from "native-base";
 import * as React from "react";
 import { Dimensions, StyleSheet, View } from "react-native";
 import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
-import MapViewDirections from "react-native-maps-directions";
-import { backgroundColor } from "react-native/Libraries/Components/View/ReactNativeStyleAttributes";
-import env from "../../../env";
+//import env from "../../../env";
 import Search from "../../utility/Search";
 
 export default function MapScreen({ navigation }) {
-    const mapurl = `https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap
-  &markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318
-  &markers=color:red%7Clabel:C%7C40.718217,-73.998284
-  &key=${env.map}`;
+    /**
+     *   const mapurl = `https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap
+    &markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318
+    &markers=color:red%7Clabel:C%7C40.718217,-73.998284
+    &key=${env.map}`;
+     * 
+     */
 
     React.useLayoutEffect(() => {
         navigation.setOptions({
             headerRight: () => (
                 <Center alignSelf={'center'} my={4} w="85%">
-                    
+
                     <Search />
                 </Center>
             ),
             title: "",
-            headerStyle:{
-                height:100,
-                justifyContent:'center',
-                
+            headerStyle: {
+                height: 100,
+                justifyContent: 'center',
+
             },
-            headerBackTitleStyle:{
+            headerBackTitleStyle: {
                 paddingVertical: 4,
             }
         });
