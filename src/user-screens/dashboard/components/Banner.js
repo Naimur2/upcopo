bb31701882bb73986a2bdb88d0adbfdc1bcc3f5b
naimur2 <naimurrahaman248@gmail.com>
2022-01-28 18:34:23 +0600

    updates

--- src/user-screens/dashboard/components/Banner.js
+++ src/user-screens/dashboard/components/Banner.js
@@ -1,11 +1,40 @@
 import { Image, Pressable, Stack, Text } from "native-base";
 import React from "react";
-import { Image as RnImage } from "react-native";
-const imageSrc = require("../../../../assets/images/countryside.jpg");
-
-const imgUri = RnImage.resolveAssetSource(imageSrc).uri;
+import usePlaceBid from '../../../hooks/usePlaceBid';
+import PlaceBidAction from "../../common/PlaceBidAction";
+const bannar=[{
+    _id: "1",
+    houseName: "Turn Key House",
+    expiresAt: "Jan 24, 2022 18:00:30",
+    currentBid: "4.33",
+    owner: "Luckas",
+    builtAt: "1942",
+    address: "1523 N 106th St, Mesa, AZ 85207",
+    sqrfit: " 3,160sqft",
+    image: "https://image.freepik.com/darmowe-wektory/wiejski-krajobraz-z-domami_23-2148669788.jpg",
+    description:
+        "Simple house with modern architecture and cool interiors located in the city crnter making easier for you to access",
+    bed: 3,
+    bath: 4,
+    createdAt: "Jan 22, 2022 18:00:30",
+    mapImage:
+        "https://miro.medium.com/max/1200/1*qYUvh-EtES8dtgKiBRiLsA.png",
+    topBid: 0.8,
+    lastBid: 0.7,
+    likes: Math.round(Math.random() * 1 + 100),
+    minimumBid: 0.541455,
+    userId: "111",
+    collectionId: "fgvhfhgf",
+    latitude: 40.724066,
+    longitude: 285.999418,
+}]
 
 export default function Banner() {
+    const houseId=bannar[0]._id;
+    const minimumBid=bannar[0].minimumBid;
+
+    const [bidHistory, showActions, showActionHandler, closeActionHandler] =
+    usePlaceBid(houseId);
     return (
         <Stack
             mb={6}
@@ -29,7 +58,14 @@ export default function Banner() {
                 >
                     Find a lender who can offer competitive
                 </Text>
-                <Pressable alignSelf={'flex-start'} bg="#fff"  px={5} borderRadius={10} py={2}>
+                <Pressable
+                    alignSelf={"flex-start"}
+                    bg="#fff"
+                    px={5}
+                    borderRadius={10}
+                    py={2}
+                    onPress={showActionHandler}
+                >
                     <Text
                         textAlign={"center"}
                         fontFamily={"body"}
@@ -44,10 +80,19 @@ export default function Banner() {
             <Image
                 position={"absolute"}
                 zIndex={-1}
-                source={{ uri: imgUri }}
+                source={{
+                    uri: "https://image.freepik.com/darmowe-wektory/wiejski-krajobraz-z-domami_23-2148669788.jpg",
+                }}
                 h="full"
                 w="full"
                 alt="cover image"
+            />
+            <PlaceBidAction
+                isOpen={showActions}
+                bidHistoryState={bidHistory}
+                onClose={closeActionHandler}
+                houseId={houseId}
+                minimumBid={minimumBid}
             />
         </Stack>
     );
